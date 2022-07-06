0a23f78e315703b49e72ae917f8f616f6b38f979
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-07-06 20:27:40 +0300

    create index views

--- multiusersapp/views.py
+++ multiusersapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from .forms import LoginForm, SignUpForm
 
 # Create your views here.
+def index(request):
+    render(request,'index.html')
