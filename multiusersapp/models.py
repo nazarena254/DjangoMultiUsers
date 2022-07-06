e35a17f59f54bbc38ac8cd60ee74bec1dfe52566
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-07-06 19:16:14 +0300

    create model

--- multiusersapp/models.py
+++ multiusersapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    is_admin=models.BooleanField('Is admin', default=False)
+    is_student = models.BooleanField('Is Student', default=False)
